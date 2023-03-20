01a5b3ceae179d5b7698653747868685e61bbea3
Elsaadany427 <hamadabassem427@gmail.com>
2023-03-20 13:01:57 +0200

    created files

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// Css inside the components folder
+import "./Components/Styles.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
