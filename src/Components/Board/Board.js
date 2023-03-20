f13360ccd17496013cd81aa893beb0a6986a2446
Elsaadany427 <hamadabassem427@gmail.com>
2023-03-20 16:26:39 +0200

    Finished Declaring a winner

--- src/Components/Board/Board.js
+++ src/Components/Board/Board.js
@@ -3,6 +3,8 @@ import { Square } from "../index";
 import { useState } from "react";
 
 export default function Board() {
+  // states to store the squers values and check the
+  // next step for x
   const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
   return (
@@ -24,13 +26,44 @@ export default function Board() {
       </div>
     </>
   );
+  // function to handle click if one of the two players has won
+  // or all the squers have completed
   function handleClick(i) {
-    if (squares[i]) {
+    if ((squares[i], checkWinner(squares))) {
       return;
     }
     const nextSquare = squares.slice();
     nextSquare[i] = xIsNext ? "X" : "O";
     setSquares(nextSquare);
     setXIsNext(!xIsNext);
+
+    const winner = checkWinner(squares);
+    let status = winner
+      ? "Winner: " + winner
+      : "Next player: " + (xIsNext ? "X" : "O");
+  }
+  // function to check the winner
+  function checkWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      ) {
+        return squares[a];
+      }
+    }
+    return null;
   }
 }
