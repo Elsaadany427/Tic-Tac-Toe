fb9f1f92e4c8d9800bc32843b2486c962a95e9ab
Elsaadany427 <hamadabassem427@gmail.com>
2023-03-20 13:45:29 +0200

    created Square folder for square component

---
+++ src/Components/Square/Square.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Square({number}) {
+  return <button className="square">{number}</button>;
+}
--- src/Components/index.js
+++ src/Components/index.js
@@ -1,2 +1,4 @@
 import Board from "./Board/Board";
-export { Board };
+import Square from "./Square/Square";
+
+export { Board, Square };
